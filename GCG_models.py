0913460d2f0a5d5cf40c9b0be59e3380a0eddae2
Francesco Bianco <francesco.bianco.5@studenti.unipd.it>
2020-07-27 10:30:16 +0200

    Copy-pasted an example from the internet, need to study it

--- GCG_models.py
+++ GCG_models.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def simpleGAN_build_generator(input_shape, img_len):
     layer_input = tf.keras.Input(input_shape)
     layers = tf.keras.layers.Flatten() (layer_input)
-    layers = tf.keras.layers.Dense(256, input_dim=input_shape) (layers)
+    layers = tf.keras.layers.Dense(256) (layers)
     layers = tf.keras.layers.LeakyReLU(alpha=0.2) (layers)
     layers = tf.keras.layers.Dense(512) (layers)
     layers = tf.keras.layers.LeakyReLU(alpha=0.2) (layers)
@@ -18,17 +18,18 @@ def simpleGAN_build_generator(input_shape, img_len):
     return model
 
 
-def simpleGAN_build_discriminator(input_shape, img_shape):
-    layer_input = tf.keras.Input(input_shape)
+def simpleGAN_build_discriminator(img_len):
+    layer_input = tf.keras.Input(img_len)
     layers = tf.keras.layers.Flatten() (layer_input)
-    layers = tf.keras.layers.Dense(1024, input_dim=input_shape) (layers)
+    layers = tf.keras.layers.Dense(1024) (layers)
     layers = tf.keras.layers.LeakyReLU(alpha=0.2) (layers)
     layers = tf.keras.layers.Dropout(rate=0.5) (layers)
     layers = tf.keras.layers.Dense(512) (layers)
     layers = tf.keras.layers.LeakyReLU(alpha=0.2) (layers)
-    layers = tf.keras.layers.Dense(256) (layers) (layers)
+    layers = tf.keras.layers.Dense(256) (layers)
     layers = tf.keras.layers.LeakyReLU(alpha=0.2) (layers)
     layers = tf.keras.layers.Dense(1, activation='sigmoid') (layers)
+    #layers = tf.keras.layers.Reshape((img_len/2, img_len/2)) (layers)
 
     model = tf.keras.models.Model(layer_input, layers)
     return model
