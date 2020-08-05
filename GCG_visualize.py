e1cbf2e83f40e10579463d1677f112aacfb8b511
Francesco Bianco <francesco.bianco.5@studenti.unipd.it>
2020-08-05 22:44:28 +0200

    Added plots creation in draft.

--- GCG_visualize.py
+++ GCG_visualize.py
@@ -1,6 +1,8 @@
+import math
 import os
 import sys
 
+from et_xmlfile import xmlfile
 from tabulate import tabulate
 from scipy.io import loadmat
 import numpy as np
@@ -137,23 +139,55 @@ for path in aae_paths:
         print("WARNING: model folder '{}' does not exist. This model will not be loaded.".format(path), file=sys.stderr)
 print("done. Loaded {} models:".format(len(models)), list(models.keys()))
 
-aae = tf.keras.models.Sequential([models['AAE_encoder'], models['AAE_decoder']])
+aae = tf.keras.models.Sequential([models['AAE_encoder'], models['AAE_decoder']], name='MNIST_AAE')
 out = aae(aae_noise).numpy()
 print(out.shape)
+x_ax = max([int(math.sqrt(out.shape[0])), 1])
+y_ax = out.shape[0] - x_ax
+plt.suptitle("Output of {}".format(aae.name))
+for i in range(out.shape[0]):
+    plt.subplot(x_ax, y_ax, 1 + i)
+    plt.imshow(out[i], cmap='gray_r')
+plt.show()
+plt.close('all')
 
-emnist_aae = tf.keras.Sequential([models['EMNIST_AAE_encoder'], models['EMNIST_AAE_decoder']])
-out = aae(aae_noise).numpy()
+emnist_aae = tf.keras.Sequential([models['EMNIST_AAE_encoder'], models['EMNIST_AAE_decoder']], name='EMNIST_AAE')
+out = emnist_aae(aae_noise).numpy()
 print(out.shape)
+x_ax = max([int(math.sqrt(out.shape[0])), 1])
+y_ax = out.shape[0] - x_ax
+plt.suptitle("Output of {}".format(emnist_aae.name))
+for i in range(out.shape[0]):
+    plt.subplot(x_ax, y_ax, 1 + i)
+    plt.imshow(out[i], cmap='gray_r')
+plt.show()
+plt.close('all')
 
 dcgan_generator = models['DCGAN_generator']
 dcgan_discriminator = models['DCGAN_discriminator']
 out = dcgan_generator(dcgan_noise).numpy()
 out = out.reshape(out.shape[0], out.shape[1], out.shape[2])
 print(out.shape)
+x_ax = max([int(math.sqrt(out.shape[0])), 1])
+y_ax = out.shape[0] - x_ax
+plt.suptitle("Output of {}".format(dcgan_generator.name))
+for i in range(out.shape[0]):
+    plt.subplot(x_ax, y_ax, 1 + i)
+    plt.imshow(out[i], cmap='gray_r')
+plt.show()
+plt.close('all')
 
 emnist_dcgan_generator = models['EMNIST_DCGAN_generator']
 emnist_dcgan_discriminator = models['EMNIST_DCGAN_discriminator']
 out = emnist_dcgan_generator(dcgan_noise).numpy()
 out = out.reshape(out.shape[0], out.shape[1], out.shape[2])
 print(out.shape)
+x_ax = max([int(math.sqrt(out.shape[0])), 1])
+y_ax = out.shape[0] - x_ax
+plt.suptitle("Output of {}".format(emnist_dcgan_generator.name))
+for i in range(out.shape[0]):
+    plt.subplot(x_ax, y_ax, 1 + i)
+    plt.imshow(out[i], cmap='gray_r')
+plt.show()
+plt.close('all')
 
