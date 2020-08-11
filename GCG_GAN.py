93da088cd6efde952456ba77cde8a5ed5651d617
frankieb96 <france.b.1996@gmail.com>
2020-08-11 09:36:27 +0200

    Updated GCG_emnist_DCGAN.py. All the files are more consistent but a proof-read is required.

--- GCG_GAN.py
+++ GCG_GAN.py
@@ -42,11 +42,11 @@ tf.random.set_seed(1)
 latent_dimension = 100
 img_shape = (28, 28, 1)
 batch_size = 32
-n_epochs = 3
+n_epochs = 30
 if len(sys.argv) < 3:
     print("WARNING: not enough input params. Resorting to default params. Usage is 'net-name' 'dataset'",
           file=sys.stderr)
-    NAME = "DCGAN"
+    NAME = "M_DCGAN"
     DATASET = "MNIST"
 else:
     NAME = sys.argv[1]
--- GCG_emnist_DCGAN.py
+++ GCG_emnist_DCGAN.py
@@ -1,3 +1,5 @@
+import sys
+
 from scipy.io import loadmat
 import os
 from tabulate import tabulate
@@ -43,90 +45,117 @@ def DCGAN_build_discriminator(img_shape=(28, 28, 1), name='EMNIST_DCGAN_discrimi
 
 """ ========================================================================================================== """
 
-batch_size = 32
-n_epochs = 3
 tf.random.set_seed(1)
 latent_dimension = 100
 img_shape = (28, 28, 1)
-DATA_TYPE = "emnist-letters"
-PATH = "temp_project/"
+batch_size = 32
+n_epochs = 30
+if len(sys.argv) < 3:
+    print("WARNING: not enough input params. Resorting to default params. Usage is 'net-name' 'dataset'",
+          file=sys.stderr)
+    NAME = "E_DCGAN"
+    DATASET = "EMNIST"
+else:
+    NAME = sys.argv[1]
+    DATASET = sys.argv[2]
+PATH = "temp_project/{}/".format(NAME)
+print("PARAMS are: ", [NAME, DATASET])
 
-print("Loading data {}...".format(DATA_TYPE), end=' ')
-mat = loadmat("temp_project/matlab/{}.mat".format(DATA_TYPE))
-data = mat['dataset']
-x_train = data['train'][0, 0]['images'][0, 0]
-y_train = data['train'][0, 0]['labels'][0, 0]
-x_test = data['test'][0, 0]['images'][0, 0]
-y_test = data['test'][0, 0]['labels'][0, 0]
-
-x_train = x_train.reshape((x_train.shape[0], 28, 28, 1), order='F')
-y_train = y_train.reshape(y_train.shape[0])
-x_test = x_test.reshape((x_test.shape[0], 28, 28, 1), order='F')
-y_test = y_test.reshape(y_test.shape[0])
-print("done.")
-
-""" NORMALIZATION """
-print("Normalizing data...", end=' ')
-x_train = (x_train / 255).astype(np.float32)
-x_test = (x_test / 255).astype(np.float32)
+""" LOADING DATASET """
+print("\nLoading {} dataset...".format(DATASET), end=' ')
+if DATASET == 'MNIST':
+    (x_train, y_train), (x_test, y_test) = GCG_utils.get_MNIST(conv_reshape=True)
+elif DATASET == 'EMNIST':
+    (x_train, y_train), (x_test, y_test) = GCG_utils.get_EMNIST(conv_reshape=True)
+else:
+    raise ValueError("Don't know {} dataset. Program now quits.".format(DATASET))
 print("done.")
 
 # see memory footprint
-print("Memory footprint:")
-mb = lambda b: "{:.2f}".format(b / (1042 ** 2))
-headers = ["", "", "shape", "data type", "bytes", "Megabytes"]
-table = [["Training set", "x_train", x_train.shape, x_train.dtype, x_train.nbytes, mb(x_train.nbytes)],
-         ["", "y_train", y_train.shape, y_train.dtype, y_train.nbytes, mb(y_train.nbytes)],
-         [],
-         ["Test set", "x_test", x_test.shape, x_test.dtype, x_test.nbytes, mb(x_test.nbytes)],
-         ["", "y_test", y_test.shape, y_test.dtype, y_test.nbytes, mb(y_test.nbytes)]]
-print(tabulate(table, headers=headers))
-print("")
+GCG_utils.print_memory_footprint(x_train, y_train, x_test, y_test)
 
 """ BUILDING THE MODELS """
 print("Building the GAN models...", end=' ')
-generator_model = DCGAN_build_generator(latent_dimension)
-discriminator_model = DCGAN_build_discriminator(img_shape)
-dcgan_model = tf.keras.models.Sequential([generator_model, discriminator_model], name='EMNIST_DCGAN')
+generator_model = DCGAN_build_generator(latent_dimension, name='{}_generator'.format(NAME))
+discriminator_model = DCGAN_build_discriminator(img_shape, name='{}_discriminator'.format(NAME))
+gan_model = tf.keras.models.Sequential([generator_model, discriminator_model], name='{}_gan'.format(NAME))
 
+optimizer = tf.keras.optimizers.Adam(0.0002, 0.5)
 discriminator_model.compile(
-    optimizer='adam',
-    loss='binary_crossentropy'
+    optimizer=optimizer,
+    loss='binary_crossentropy',
+    metrics=['accuracy']
 )
 discriminator_model.trainable = False
 
-dcgan_model.compile(
-    optimizer='adam',
-    loss='binary_crossentropy'
+gan_model.compile(
+    optimizer=optimizer,
+    loss='binary_crossentropy',
+    metrics=['accuracy']
 )
 print("done.", flush=True)
 
+
 """ TRAIN THE MODEL IF IT DOES NOT EXIST """
-end_epoch_noise = tf.random.normal(shape=[25, latent_dimension])
 dataset = tf.data.Dataset.from_tensor_slices(x_train).shuffle(1000)
 dataset = dataset.batch(batch_size, drop_remainder=True).prefetch(1000)
-if not os.path.exists("temp_project/" + dcgan_model.name):
-    print("Folder '{}'".format(dcgan_model.name), "has not been found: training the model over", n_epochs, "epochs.")
-    os.makedirs(PATH + dcgan_model.name + "/" + discriminator_model.name)
-    os.makedirs(PATH + dcgan_model.name + "/" + generator_model.name)
-    os.makedirs(PATH + dcgan_model.name + "/train_images/")
-    GCG_utils.train_DCGAN(dcgan_model, generator_model, discriminator_model, dataset,
-                          int(x_train.shape[0] / batch_size), latent_dimension, batch_size, n_epochs)
+if not os.path.exists(PATH):
+    print("Folder '{}'".format(PATH), "has not been found: training the model over", n_epochs, "epochs.")
+    os.makedirs(PATH)
+    os.makedirs(PATH + discriminator_model.name)
+    os.makedirs(PATH + generator_model.name)
+    os.makedirs(PATH + "train_images/")
+    epoch_history_discriminator, epoch_history_gan = GCG_utils.train_DCGAN(gan_model, generator_model, discriminator_model, dataset, int(x_train.shape[0] / batch_size), latent_dimension, batch_size, n_epochs, path=PATH)
+
 else:
-    print("Folder '{}'".format(dcgan_model.name), "has been found: loading model, no need to retrain.")
+    print("Folder '{}' has been found: loading model, no need to retrain.".format(NAME))
     generator_model = tf.keras.models.load_model(PATH + generator_model.name)
     discriminator_model = tf.keras.models.load_model(PATH + discriminator_model.name)
-    dcgan_model = tf.keras.models.Sequential([generator_model, discriminator_model], name="EMNIST_DCGAN")
+    gan_model = tf.keras.models.Sequential([generator_model, discriminator_model], name="{}_gan".format(NAME))
+    with np.load(PATH + "training.npz") as load:
+        epoch_history_discriminator = load['discr']  # loss, accuracy
+        epoch_history_gan = load['gan']  # loss, accuracy
+
 
 """ SEE RESULTS """
+# plot losses
+plt.figure(figsize=(16, 5))
+plt.subplot(1, 2, 1)
+plt.plot(epoch_history_discriminator[:, 0])
+plt.title("{} loss".format(discriminator_model.name))
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.subplot(1, 2, 2)
+plt.plot(epoch_history_gan[:, 0])
+plt.title("{} loss".format(gan_model.name))
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+
+plt.show()
+
+# plot accuracies
+plt.figure(figsize=(16, 5))
+plt.subplot(1, 2, 1)
+plt.plot(epoch_history_discriminator[:, 1])
+plt.title("{} accuracy".format(discriminator_model.name))
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.subplot(1, 2, 2)
+plt.plot(epoch_history_gan[:, 1])
+plt.title("{} accuracy".format(gan_model.name))
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+
+plt.show()
+
 # plot images
 for i in range(5):
     noise = tf.random.normal(shape=[25, latent_dimension])
-    fake_images = generator_model(noise).numpy()
+    fake_images = generator_model(noise).numpy().reshape([25, 28, 28])
     for i in range(25):
         # define subplot
         plt.subplot(5, 5, 1 + i)
         plt.axis('off')
-        plt.imshow(fake_images[i].reshape(28, 28), cmap='gray_r')
+        plt.imshow(fake_images[i], cmap='gray_r')
     plt.show()  # see the results
     plt.close('all')
